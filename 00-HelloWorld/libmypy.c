ec0bcdbeec7c42a05ef999f6a24c4e3810e6a2f3
Stephen Polcyn <stephen@reverielabs.com>
2022-07-13 22:50:39 +0000

    Add example of blocking with sleep

--- 00-HelloWorld/libmypy.c
+++ 00-HelloWorld/libmypy.c
@@ -1,6 +1,10 @@
 #include <Python.h>
 #include "libmypy.h"
+#include <stdio.h>
 
 PyObject * hello(PyObject * self) {
+    // Py_BEGIN_ALLOW_THREADS
+    sleep(500);
+    // Py_END_ALLOW_THREADS
 	return PyUnicode_FromFormat("Hello C extension!");
 }
--- 00-HelloWorld/test.py
+++ 00-HelloWorld/test.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
 import helloworld
+import asyncio
 
-print(helloworld.hello());
-help(helloworld);
+
+async def main():
+    print("running")
+
+    def fn():
+        print(helloworld.hello())
+
+    task = asyncio.create_task(
+        asyncio.wait_for(
+            asyncio.get_running_loop().run_in_executor(None, fn),
+            timeout=None,
+        )
+    )
+
+    while True:
+        print("preparing to sleep")
+        await asyncio.sleep(1)
+        print("finished sleeping")
+        if task.done():
+            task.result()
+
+
+asyncio.run(main())
